f98039cbbec2c2e4a2c90ee78dc2a0faa9c3b27e
Ahmed <ahmed.raza1054@gmail.com>
2023-08-07 02:46:53 +0500

    javaScript assignment
    first assignment of javaScript

---
+++ JavaScript-Assignment/1-Basic/app.js
@@ -0,0 +1,228 @@
+// JavaScript Assignment 01
+
+//                         <<<<<<<<<<<<<<<<<< Chapter No 01 >>>>>>>>>>>>>>>>>>
+
+// Question No 01
+
+// alert("Welcome");
+
+// Question No 02
+
+// alert("Error! Please enter a valid password.");
+
+// Question No 03
+
+// alert("Welcome to JS Land...\n Happy Coding!");
+
+// Question No 04
+
+// alert("Welcome to JS Land...");
+// alert("Happy Coding!");
+
+// Question No 05
+
+// console.log("Hello... i can run JS through my web browser's console")
+
+//                           <<<<<<<<<<<<<<<<<< Chapter No 02 >>>>>>>>>>>>>>>>>>
+
+// Question No 01
+
+// var username= ""
+
+// alert()
+
+// Question No 02
+
+// var myName = "Ahmedraza"
+
+// alert(myName)
+
+// Question No 03
+
+// var message=""
+
+// message = "Hello world"
+
+// alert(message)
+
+// Question No 04
+
+// var studentName="Ahmed"
+
+// var studentAge="28"
+
+// var course="Certified Web & App Developer"
+
+// alert(studentName)
+// alert(studentAge)
+// alert(course)
+
+// Question No 05
+
+// var wannaEat= "Pizza\nPizz\nPiz\nPi\nP"
+
+// alert(wannaEat)
+
+// Question No 06
+
+// var msg="My email address is : "
+
+// var email="ahmedraza.1054@gmail.com"
+
+// alert(msg + email)
+
+// Question No 07
+
+// var msgBook="I am trying to learn from the book : "
+
+// var book="A smarter way to learn JavaScript."
+
+// alert(msgBook+book)
+
+// Question No 09
+
+// var showCase="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+
+// alert(showCase)
+
+//                           <<<<<<<<<<<<<<<<<< Chapter No 03 >>>>>>>>>>>>>>>>>>
+
+// Question No 01
+
+// var age="i am 28 years old"
+
+// alert(age)
+
+// Question No 02
+
+// var visitCount="Youhave visited this site 69 times."
+
+// alert(visitCount)
+
+// Question No 03
+
+// var msg="My Birth Year is "
+// var birthYear = 1995;
+
+// console.log(msg + birthYear + "\nData type of my declared variable is number");
+
+// Question No 04
+
+// var visitorName = "ahmed";
+// var productTitle = "t-shirts";
+// var quntaity = 5;
+// var store = "www.chaman.com";
+
+// console.log(visitorName + " ordered " + quntaity + productTitle + " from " + store);
+
+//                           <<<<<<<<<<<<<<<<<< Chapter No 04 >>>>>>>>>>>>>>>>>>
+
+// Question No 01
+
+// var no1 = 1,
+//   no2 = 2,
+//   no3 = 3;
+
+// alert(no1 + no2 + no3);
+
+// Question No 02
+
+// Five illegal variables
+
+// var "name"="ahmed"
+// var 1="ahmed"
+// var 1name="ahmed"
+// var full name="ahmedraza"
+// var var="hello world"
+
+// Five legal variables
+
+// var fullName=""
+// var batch10=""
+// var course$timing=""
+// var institute_name=""
+// var adress=""
+
+// Question No 03
+
+// var a = "“Rules for naming JS variables”";
+// var b =
+//   "Variable names can only contain" +
+//   "numbers," +
+//   "$ and_ " +
+//   "For example $my_1stVariable";
+// var c = "Variables must begin with a" + "$name," + "_name" + "or name";
+// var d = "Variable names are case" + " sensitive";
+// var e = "Variable names should not be JS" + " Keywards";
+
+// console.log(a + "\n" + b + "\n" + c + "\n" + d + "\n" + e);
+
+//                           <<<<<<<<<<<<<<<<<< Chapter No 05 >>>>>>>>>>>>>>>>>>
+
+// Question No 01
+// var no = 3;
+// var add = 5;
+// var msg1 = "Sum of ";
+// var msg2 = " and ";
+// var msg3 = " is ";
+
+// console.log(msg1 + no + msg2 + add + msg3 + (no + add));
+
+// Question No 02
+// var no = 3;
+// var add = 5;
+// var msg1 = "division ";
+// var msg2 = " and ";
+// var msg3 = " is ";
+
+// console.log(msg1 + no + msg2 + add + msg3 + (no / add));
+
+// var no = 3;
+// var add = 5;
+// var msg1 = "subtraction ";
+// var msg2 = " and ";
+// var msg3 = " is ";
+
+// console.log(msg1 + no + msg2 + add + msg3 + (no - add));
+
+// var no = 3;
+// var add = 5;
+// var msg1 = "modulus ";
+// var msg2 = " and ";
+// var msg3 = " is ";
+
+// console.log(msg1 + no + msg2 + add + msg3 + (no * add));
+
+// Question No 03
+
+console.log();
+// Question No 04
+
+console.log();
+// Question No 05
+
+console.log();
+// Question No 06
+
+console.log();
+// Question No 07
+
+console.log();
+// Question No 08
+
+console.log();
+// Question No 09
+
+console.log();
+// Question No 10
+
+console.log();
+// Question No 11
+
+console.log();
+// Question No 12
+
+console.log();
+// Question No 13
+
+console.log();
