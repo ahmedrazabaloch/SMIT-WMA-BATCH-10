c611c60d206a71f029d68a47f68edc19ab7c97ee
Ahmed <ahmed.raza1054@gmail.com>
2023-08-14 23:29:25 +0500

    JS Second Assignment
    JS Second Assignment

---
+++ JavaScript-Assignment/02-Chapter-06-To-13/app.js
@@ -0,0 +1,52 @@
+// Capter 6-9
+
+// Question-1
+
+// var a = 10;
+
+// document.write("Result:");
+// document.write("<br/> The Value of a is: " + a);
+// document.write("<br/>.........................");
+// document.write("<br/>");
+// document.write("<br/> The Value of ++a is: " + ++a);
+// document.write("<br/> Now the Value of a is: " + a);
+// document.write("<br/>");
+// document.write("<br/> The Value of a++ is: " + a++);
+// document.write("<br/> Now the Value of a is: " + a);
+// document.write("<br/>");
+// document.write("<br/> The Value of --a is: " + --a);
+// document.write("<br/> Now the Value of a is: " + a);
+// document.write("<br/>");
+// document.write("<br/> The Value of a-- is: " + a--);
+// document.write("<br/> Now the Value of a is: " + a);
+// document.write("<br/>");
+
+// Question-2
+
+// var a = 2,
+//   b = 1;
+
+// var result = --a - --b + ++b + b--;
+// document.write("--a pre decrement");
+// document.write("<br/> --a - --b; pre decrement");
+// document.write(
+//   "<br/> --a - --b + ++b; pre decrement concatenate with pre increment"
+// );
+// document.write(
+//   "<br/> --a - --b + ++b + b--; pre decrement concatenate with pre increment"
+// );
+// document.write("<br/> a is " + a);
+// document.write("<br/>b is " + b);
+// document.write("<br/>result is " + result);
+
+// Question-3
+// var username = prompt("Please enter your name");
+// alert("Welcome " + username);
+
+// Question-4,5
+
+// var num = prompt("write a number",5);
+// for (var i = 1; i <= 10; i++) {
+// var result = num * i;
+// document.write(num + " x " + i + " = " + result + "<br>");
+// }
