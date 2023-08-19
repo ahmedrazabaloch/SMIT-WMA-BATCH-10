cfd8cff3939c41f760a75428f4e1059a1d39d9e5
Ahmed <ahmed.raza1054@gmail.com>
2023-08-19 14:55:55 +0500

    JS Assignment 3
    JS Assignment 3 underdevelopment

---
+++ JavaScript-Assignment/03-Chapter-14-To-16/app.js
@@ -0,0 +1,59 @@
+// <<<<<<<<<<<< Chapter 13 to 15 >>>>>>>>>>>>
+
+//Questin No-01
+
+// var students = [];
+
+//Questin No-02
+
+// students = [];
+
+//Questin No-03
+
+// var str = ["Ahmed", "Raza", "Salman", "Ali"];
+
+//Questin No-04
+
+// var num = [126864, 126863, 126862, 126861];
+
+//Questin No-05
+
+// var boole = [true, false];
+
+//Questin No-06
+
+// var mixed = ["Ahmed", 126864, true, "Salman", 126863, false];
+
+//Questin No-07
+
+// var qualifictions = [
+//   "SSC",
+//   "HSC",
+//   "BCS",
+//   "BS",
+//   "BCOM",
+//   "MS",
+//   "M. Phil.",
+//   "PhD",
+// ];
+
+// document.write("<h2>Qualifications:</h2>");
+// document.write(qualifictions);
+
+//Questin No-08
+
+
+
+//Questin No-09
+
+//Questin No-10
+
+//Questin No-11
+
+//Questin No-12
+
+//Questin No-13
+
+//Questin No-14
+
+//Questin No-15
