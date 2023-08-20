cdba5cced24fd541538f14fe17c8d8f8f507fae9
Ahmed <ahmed.raza1054@gmail.com>
2023-08-21 01:58:42 +0500

    JS Assignment 03
    Completed

--- JavaScript-Assignment/03-Chapter-14-To-16/app.js
+++ JavaScript-Assignment/03-Chapter-14-To-16/app.js
@@ -2,7 +2,7 @@
 
 //Questin No-01
 
-// var students = [];
+// var students;
 
 //Questin No-02
 
@@ -76,16 +76,122 @@
 
 //Questin No-09
 
+// var colorName = ["Green"];
+// alert(colorName);
 
+// colorName.unshift(
+//   prompt("Enter a color name you want to add a beginning", "Red")
+// );
+// alert(colorName);
+
+// colorName.push(prompt("Enter a colorn name you want to add in end", "Orange"));
+// alert(colorName);
+
+// colorName.unshift(
+//   prompt("Add two more color name you want to add in in beginning", "Black")
+// );
+// colorName.unshift(
+//   prompt("Add two more color name you want to add in in beginning", "White")
+// );
+// alert(colorName);
+
+// colorName.shift();
+// alert(colorName);
+
+// colorName.pop();
+// alert(colorName);
+
+// colorName.push(prompt("which index you want to add a color name.", "2"));
+// alert(colorName);
+
+// colorName.splice(
+//   prompt(
+//     "Which index you want to delete color(name) & how many colors want to delete",
+//     "2,2"
+//   )
+// );
+// alert(colorName);
+
+// document.write(colorName);
 
 //Questin No-10
 
+// var studentScore = [320, 230, 480, 120];
+// document.write("Scores of Students : " + studentScore);
+// document.write("<br/>Ordered Scores of Students : " + studentScore.sort());
+
 //Questin No-11
+
+// document.write("Cities list:");
+
+// var city = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar"];
+// document.write("<br/>" + city);
+// document.write("<br/>");
+// document.write("<br/> Selected cities list:");
+
+// var selectedCity = city.slice(2, 4);
+// document.write("<br/>" + selectedCity);
 
 //Questin No-12
 
-//Questin No-13
+// var arr = ["This ", " is ", " my ", " cat"];
+
+// document.write("Array" + "<br/>" + arr);
+
+// document.write("<br/>");
+
+// document.write("<br/> String:" + "<br/>" + arr.join(""));
+
+// //Questin No-13
+
+// var device = ["Keyboard", "Mouse", "Printer", "Monitor"];
+
+// document.write("Devices:" + "<br/>" + device);
+// document.write("<br/>");
+
+// var returnValue = device.shift();
+// document.write("<br/> Out:" + "<br/>" + returnValue);
+
+// var returnValue = device.shift();
+// document.write("<br/> Out:" + "<br/>" + returnValue);
+
+// var returnValue = device.shift();
+// document.write("<br/> Out:" + "<br/>" + returnValue);
+
+// var returnValue = device.shift();
+// document.write("<br/> Out:" + "<br/>" + returnValue);
 
 //Questin No-14
 
+// var device = ["Keyboard", "Mouse", "Printer", "Monitor"];
+
+// document.write("Devices:" + "<br/>" + device);
+// document.write("<br/>");
+
+// var returnValue = device.pop();
+// document.write("<br/> Out:" + "<br/>" + returnValue);
+
+// var returnValue = device.pop();
+// document.write("<br/> Out:" + "<br/>" + returnValue);
+
+// var returnValue = device.pop();
+// document.write("<br/> Out:" + "<br/>" + returnValue);
+
+// var returnValue = device.pop();
+// document.write("<br/> Out:" + "<br/>" + returnValue);
+
 //Questin No-15
+
+// var phoneManufacturers = [
+//   "Apple",
+//   "Samsung",
+//   "Motorola",
+//   "Nokia",
+//   "Sony",
+//   "Haier",
+// ];
+
+// document.write("Select a Phone Manufacturer:");
+// document.write(
+//   "<select>" + "<option>" + phoneManufacturers[0] + "</option>" + "</select>"
+// );
