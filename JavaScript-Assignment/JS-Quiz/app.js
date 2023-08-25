b0f3f2fedf24f6e5a8300bdc4f8073ed11d37c6f
Ahmed <ahmed.raza1054@gmail.com>
2023-08-26 02:16:56 +0500

    JavaScript Quiz Question & Answer
    JavaScript Quiz Question & Answer

---
+++ JavaScript-Assignment/JS-Quiz/app.js
@@ -0,0 +1,54 @@
+// Palindrome words
+
+// var userInput = prompt("Enter a word");
+
+// var check = "";
+
+// for (var i = userInput.length - 1; i >= 0; i--) {
+//   check += userInput[i];
+// }
+
+// if (userInput === check) {
+//   alert(userInput + " Its a palindrome word");
+// } else {
+//   alert("Try again for palindrome word");
+// }
+
+// Missing Number Found
+
+// var num = [20, 21, 22, 23, 25,26,27,28,30];
+
+// for (var i = 0; i < num.length; i++) {
+//   if (num[i + 1] - num[i] ===2 ) {
+//     console.log(num[i]+1);
+//   }
+// }
+
+// Sorting Even & Odd Number
+
+// var numCheck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (var i = 0; i < numCheck.length; i++) {
+//   if (numCheck[i] % 2 === 0) {
+//     console.log(numCheck[i]);
+//   }
+// }
+
+// Max & Min Number
+
+var number = [4, 8, 16, 40, 12, 3, 5];
+
+var max = number[0];
+
+var min = number[0];
+
+for (let i = 0; i < number.length; i++) {
+  if (number[i] > max) {
+    max = number[i];
+  }
+
+  if (number[i] < min) {
+    min = number[i];
+  }
+}
+console.log("Maximum Number = " + max);
+console.log("Minimum Number = " + min);
