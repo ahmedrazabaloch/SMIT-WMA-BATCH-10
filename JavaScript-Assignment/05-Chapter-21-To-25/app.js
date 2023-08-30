47dba4716a6fe64f8fe652343b112b1fdc95dce5
Ahmed <ahmed.raza1054@gmail.com>
2023-08-31 03:19:14 +0500

    JS Assignment 5 Chapter 21 to 25
    JS Assignment 5 Chapter 21 to 25
    13 question solve

---
+++ JavaScript-Assignment/05-Chapter-21-To-25/app.js
@@ -0,0 +1,117 @@
+//                                           <<<<<<<<<<<<<< Chapter 21 to 25 >>>>>>>>>>>>>
+
+//                                                          Question No- 01
+// var firstName = prompt("Please Enter Your First Name");
+// var lastName = prompt("Please Enter Your Last Name");
+// var fullName = firstName + " " + lastName;
+
+// document.write("Welcome to SMIT Sir " + fullName);
+
+//                                                          Question No- 02
+
+// var userInput = prompt("Please enter you favorite mobile name and model");
+
+// document.write("My Favorite phone is " + userInput);
+// document.write("<br/>Lenght of string " + userInput.length);
+
+//                                                          Question No- 03
+
+// var str = "Pakistani";
+// document.write("String: " + str);
+// document.write("<br/>Index of word 'N': " + str.indexOf("n"));
+
+//                                                          Question No- 04
+
+// var str = "Hello world";
+
+// document.write("String: " + str);
+// document.write("<br/>Last Index of word 'L': " + str.lastIndexOf("l"));
+
+//                                                          Question No- 05
+
+// var str = "Pakistani";
+// document.write("String: " + str);
+// document.write("<br/>Character at index 3: " + str.charAt(3));
+
+//                                                          Question No- 06
+
+// var firstName = prompt("Please Enter Your First Name");
+// var lastName = prompt("Please Enter Your Last Name");
+
+// document.write("Welcome to SMIT Sir " + firstName.concat(" " + lastName));
+
+//                                                          Question No- 07
+
+// var city="Hyderabad";
+
+// document.write("City "+ city)
+// document.write("<br/>After replacement: "+ city.replace("Hyder","Islam"))
+
+//                                                          Question No- 08
+
+// var message =
+//   "Ali and Sami are best friends. They play cricket and football together.";
+
+//   document.write(message.replaceAll("and","&"))
+
+//                                                          Question No- 09
+
+// var value = "472";
+
+// document.write("Value: " + value);
+// document.write("<br/>Type: " + typeof value);
+// document.write("<br/>Value: " + value);
+// document.write("<br/>Type: " + typeof parseInt(value));
+
+//                                                          Question No- 10
+
+// var userInput=prompt("Enter a word")
+
+// document.write("User input: " + userInput)
+// document.write("<br/>Upper case: " + userInput.toUpperCase())
+
+//                                                          Question No- 11
+
+// var userInput = prompt("Enter a word");
+
+// document.write("User input: " + userInput);
+// document.write("<br/>Title case: " + userInput.charAt(0).toUpperCase() + userInput.slice(1));
+
+//                                                          Question No- 12
+
+// var num = 35.36;
+
+// document.write("Number: " + num);
+// document.write("<br/>Result: " + num.toString().replace('.',''));
+
+//                                                          Question No- 13
+
+//                                                          Question No- 14
+
+// var userInput = prompt(
+//   "Welcome to Che Bakery whats your order Ma'am | Sir"
+// ).toLowerCase();
+// var item = ["cake", "apple pie", "cookie", "chips", "patties"];
+
+// var flag = true;
+
+// for (var i = 0; i < item.length; i++) {
+//   if (userInput == item[i]) {
+//     document.write(
+//       userInput + " is availabe at index " + [i] + " in our bakery"
+//     );
+//     flag = false;
+//     break;
+//   }
+// }
+// if (flag) {
+//   document.write("We are sorry " + userInput + " is not availbe in our bakery");
+// }
+
+//                                                          Question No- 15
+
+//                                                          Question No- 16
+
+//                                                          Question No- 17
+
+//                                                          Question No- 18
