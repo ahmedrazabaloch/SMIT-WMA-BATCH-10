d1f1dfdfe461cb5fc0678d38d54510de32eb6ea1
Ahmed <ahmed.raza1054@gmail.com>
2023-09-07 23:48:13 +0500

    JS Assignment Chapter 31 to 34
    JS Assignment Chapter 31 to 34

---
+++ JavaScript-Assignment/07-Chapter-31-To-34/app.js
@@ -0,0 +1,65 @@
+// >>>>>>>>>>>>>>>>>>>> Chapter No 31 to 34 <<<<<<<<<<<<<<<<<<<<<
+
+// Question No=01
+
+// var date = new Date();
+
+// document.write(date);
+
+// Question No=02
+
+// monthNames = [
+//   "January",
+//   "February",
+//   "March",
+//   "April",
+//   "May",
+//   "June",
+//   "July",
+//   "August",
+//   "September",
+//   "October",
+//   "November",
+//   "December",
+// ];
+
+// var date = new Date();
+
+// var month = monthNames[date.getMonth()];
+
+// document.write("Current month: "+month);
+
+// Question No=03
+
+// var daysName = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+// var date = new Date();
+
+// var day = daysName[date.getDay()];
+
+// document.write("Today is: " + day);
+
+// Question No=04
+
+// var daysName = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+// var date = new Date();
+
+// var day = daysName[date.getDay()];
+
+// if (day == "Sun" || day == "Sat") {
+//   document.write("It's Fun day");
+// } else {
+//   document.write("Today is: " + day);
+// }
+
+// Question No=05
+// Question No=06
+// Question No=07
+// Question No=08
+// Question No=09
+// Question No=10
+// Question No=11
+// Question No=12
+// Question No=13
+// Question No=14
