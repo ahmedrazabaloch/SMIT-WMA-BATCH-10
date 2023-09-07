b027b09eaaa5fc0db722d180f04576c92384d52d
Ahmed <ahmed.raza1054@gmail.com>
2023-09-07 17:27:55 +0500

    JS Assignment chapter 26 to 30 complete
    JS Assignment chapter 26 to 30 complete

---
+++ JavaScript-Assignment/06-Chapter-26-To-30/app.js
@@ -0,0 +1,94 @@
+// >>>>>>>>>>>>>>>>>>>>>> Chapter 26 to 30 <<<<<<<<<<<<<<<<<<<<<<<
+
+// Question No 01
+
+// var num = 3.45214;
+
+// var round = Math.round(num);
+
+// var floor = Math.floor(num);
+
+// var ceil = Math.ceil(num);
+
+// document.write(
+//   "Number: " +
+//     num +
+//     "<br/> rond off value: " +
+//     round +
+//     "<br/> floor value: " +
+//     floor +
+//     "<br/> ceil value: " +
+//     ceil
+// );
+
+// Question No 02
+
+// var num = -2.673;
+
+// var round = Math.round(num);
+
+// var floor = Math.floor(num);
+
+// var ceil = Math.ceil(num);
+
+// document.write(
+//   "Number: " +
+//     num +
+//     "<br/> rond off value: " +
+//     round +
+//     "<br/> floor value: " +
+//     floor +
+//     "<br/> ceil value: " +
+//     ceil
+// );
+
+// Question No 03
+
+// var num = -4;
+
+// var absolute = Math.abs(num);
+
+// document.write("The absolute value of " + num + " is " + absolute);
+
+// Question No 04
+
+// var random = Math.floor(Math.random() * 10);
+
+// document.write("random dice value: " + random);
+
+// Question No 05
+
+// var random = Math.ceil(Math.random() * 2);
+
+// if (random === 2) {
+//   document.write(random + "<br/> random coin value: Heads ");
+// } else {
+//   document.write(random + "<br/> random coin value: Tails ");
+// }
+
+// Question No 06
+
+// var random = Math.ceil(Math.random() * 100);
+
+// document.write("random number between 1 and 100: " + random);
+
+// Question No 07 need to be check
+
+// var userInput = prompt("Enter your weight", "84.62");
+
+// var kgs = userInput;
+
+// document.write("The weight of user is " + kgs + " kilograms");
+
+// Question No 08
+
+// var random = Math.ceil(Math.random() * 10);
+
+// var userInput = +prompt("Enter a number");
+
+// if (random === userInput) {
+//   alert("Congratulate you guess the right one");
+// } else {
+//   alert("try again");
+// }
+// console.log(random);
