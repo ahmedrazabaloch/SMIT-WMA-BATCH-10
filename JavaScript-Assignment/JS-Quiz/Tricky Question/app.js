d2e638043402206eea8f20b6cfdc54f1556b07fa
Ahmed <ahmed.raza1054@gmail.com>
2023-09-09 01:50:53 +0500

    tricky question file update
    tricky question file update

--- JavaScript-Assignment/JS-Quiz/Tricky Question/app.js
+++ JavaScript-Assignment/JS-Quiz/Tricky Question/app.js
@@ -1,4 +1,4 @@
-// Palindrome words
+//                                                   >>>>>>>>>>>>>>>>>>> Palindrome words <<<<<<<<<<<<<<<<<<<<<<<
 
 // var userInput = prompt("Enter a word");
 
@@ -14,7 +14,7 @@
 //   alert("Try again for palindrome word");
 // }
 
-// Missing Number Found
+//                                                     >>>>>>>>>>>>> Missing Number Found <<<<<<<<<<<<<
 
 // var num = [20, 21, 22, 23, 25,26,27,28,30];
 
@@ -24,7 +24,7 @@
 //   }
 // }
 
-// Sorting Even & Odd Number
+//                                              >>>>>>>>>>>>>>>>>>>> Sorting Even & Odd Number <<<<<<<<<<<<<<<<<<<<<<<<<
 
 // var numCheck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // for (var i = 0; i < numCheck.length; i++) {
@@ -33,7 +33,7 @@
 //   }
 // }
 
-// Max & Min Number
+//                                                 >>>>>>>>>>>>>>>>>> Max & Min Number <<<<<<<<<<<<<<<<<<<
 
 // var number = [4, 8, 16, 40, 12, 3, 5];
 
@@ -53,47 +53,13 @@
 // console.log("Maximum Number = " + max);
 // console.log("Minimum Number = " + min);
 
-// Swting Name & update array
+//                                          >>>>>>>>>>>>>>>>>>>>> Switching Name & update array <<<<<<<<<<<<<<<<<<<<<<<<<
 
 // var arr = ["ghous", "ahmed", "zain"];
 
-// arr[2] = [arr[0],arr[2]]
+// (arr[2] = [arr[0], (arr[0] = arr[2])]), [0];
 
 // console.log(arr)
-
-// update = text.slice(21,24);
-
-// var text =
-//   "IHC reserves verdict on Imrans plea challenging Toshakhana conviction";
-
-// var firstChar = text.slice(21,7);
-// if (firstChar !== -1) {
-//   text = text.slice(21, firstChar) + "Nawaz " + text.slice(firstChar + 0);
-// }
-
-// console.log(text);
-
-// Methods
-
-// .push()  & unshift return the orignal value with update
-
-// .push() add the value in last
-
-// var arr = ["salman", "ali"];
-
-// arr.push("ahmed");
-// console.log(arr);
-
-// // unshift() add the value in first
-// var a = ["salman", "ali"];
-
-// a.unshift("ahmed");
-// console.log(a);
-
-// var arr = ["salman", "ali"];
-
-// arr.pop();
-// console.log(arr);
 
 //                                               >>>>>>>>>>>>>>> Ramdom Password generator <<<<<<<<<<<<<<<<<
 
@@ -147,33 +113,33 @@
 // document.write("<br/>");
 // document.write("<br/> Total Minutes: => " + "    " + Math.round(minutes));
 
-//                                           >>>>>>>>>>>>>>> Birth Days Calculator <<<<<<<<<<<<<<<<<<
+//                                                   >>>>>>>>>>>>>>> Birth Days Calculator <<<<<<<<<<<<<<<<<<
 
-var ramdan = new Date("Mar 12 2024");
+// var ramdan = new Date("Mar 12 2024");
 
-var currentDate = new Date();
+// var currentDate = new Date();
 
-var milliSeconds = ramdan - currentDate;
+// var milliSeconds = ramdan - currentDate;
 
-var days = milliSeconds / 86400000;
+// var days = milliSeconds / 86400000;
 
-var hours = days * 24;
+// var hours = days * 24;
 
-var month = Math.floor(days) / 30.417;
+// var month = Math.floor(days) / 30.417;
 
-var minutes = milliSeconds / 60;
+// var minutes = milliSeconds / 60;
 
-console.log(days)
+// console.log(days)
 
-document.write("<h1>How Many Days left Ramadan 2024</h1>");
-document.write("<br/>Ramadan: => " + "    " + ramdan);
-document.write("<br/>");
-document.write("<br/> Current Date: => " + "    " + currentDate);
-document.write("<br/>");
-document.write("<br/> Month left: => " + "    " + month.toFixed(1));
-document.write("<br/>");
-document.write("<br/> Days left: => " + "    " + days.toFixed(1));
-document.write("<br/>");
-document.write("<br/> Hours left: => " + "    " + hours.toFixed(2));
-document.write("<br/>");
-document.write("<br/> Minutes left: => " + "    " + Math.round(minutes));
+// document.write("<h1>How Many Days left Ramadan 2024</h1>");
+// document.write("<br/>Ramadan: => " + "    " + ramdan);
+// document.write("<br/>");
+// document.write("<br/> Current Date: => " + "    " + currentDate);
+// document.write("<br/>");
+// document.write("<br/> Month left: => " + "    " + month.toFixed(1));
+// document.write("<br/>");
+// document.write("<br/> Days left: => " + "    " + days.toFixed(1));
+// document.write("<br/>");
+// document.write("<br/> Hours left: => " + "    " + hours.toFixed(2));
+// document.write("<br/>");
+// document.write("<br/> Minutes left: => " + "    " + Math.round(minutes));
