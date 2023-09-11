390deca58cffda9359c582488041be3cee19ab36
Ahmed <ahmed.raza1054@gmail.com>
2023-09-12 02:23:39 +0500

    JS Assignment 7 chapter 34 to 38
    JS Assignment 7 chapter 34 to 38 incomplete

---
+++ JavaScript-Assignment/08-Chapter-34-To-38/app.js
@@ -0,0 +1,75 @@
+//                                             >>>>>>>>>>>>>>>> Chapter No 34 to 38 <<<<<<<<<<<<<<<
+
+//                                              >>>>>>>>>>>>>>>>> Question No=01 <<<<<<<<<<<<<<<<<
+
+// function date() {
+//   var currentDate = new Date();
+//   console.log(currentDate);
+// }
+
+// date();
+
+//                                              >>>>>>>>>>>>>>>>> Question No=02 <<<<<<<<<<<<<<<<<
+
+// function greet(firstName, lastName) {
+//   var greeting = firstName + " "+lastName;
+//   return greeting;
+// }
+// console.log(
+//   greet(prompt("Enter your first name"), prompt("Enter your last Name"))
+// );
+
+//                                              >>>>>>>>>>>>>>>>> Question No=03 <<<<<<<<<<<<<<<<<
+
+// function calc(firstValue, secondValue) {
+//   var sum = firstValue + secondValue;
+//   return sum;
+// }
+
+// console.log(
+//   calc(+prompt("Please enter a number"), +prompt("Sum with anther number"))
+// );
+
+//                                              >>>>>>>>>>>>>>>>> Question No=04 <<<<<<<<<<<<<<<<< Opration not working need to be check
+
+// function value(num1, sing, num2) {
+//   var calc = num1 + sing + num2;
+//   return calc;
+// }
+
+// console.log(
+//   value(
+//     parseInt(prompt("Please enter a value")),
+//     prompt("Enter a opration"),
+//     parseInt(prompt("Enter a second Value"))
+//   )
+// );
+
+//                                              >>>>>>>>>>>>>>>>> Question No=05 <<<<<<<<<<<<<<<<< pending
+
+//                                              >>>>>>>>>>>>>>>>> Question No=06 <<<<<<<<<<<<<<<<< pending
+
+//                                              >>>>>>>>>>>>>>>>> Question No=07 <<<<<<<<<<<<<<<<< wrong code
+
+// function counting(start, end) {
+//   var value = 0;
+//   for (var i = start; i < end; i++) {
+//     value += value;
+//     console.log(value);
+//   }
+//   return value;
+// }
+// counting(
+//   console.log(
+//     parseInt(prompt("Enter a starting value")),
+//     parseInt(prompt("Enter a ending value"))
+//   )
+// );
+
+//                                              >>>>>>>>>>>>>>>>> Question No=08 <<<<<<<<<<<<<<<<<
+//                                              >>>>>>>>>>>>>>>>> Question No=09 <<<<<<<<<<<<<<<<<
+//                                              >>>>>>>>>>>>>>>>> Question No=10 <<<<<<<<<<<<<<<<<
+//                                              >>>>>>>>>>>>>>>>> Question No=11 <<<<<<<<<<<<<<<<<
+//                                              >>>>>>>>>>>>>>>>> Question No=12 <<<<<<<<<<<<<<<<<
+//                                              >>>>>>>>>>>>>>>>> Question No=13 <<<<<<<<<<<<<<<<<
+//                                              >>>>>>>>>>>>>>>>> Question No=14 <<<<<<<<<<<<<<<<<
