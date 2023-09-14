efba74174d3ced7b0f011998dba35ccab459e82a
Ahmed <ahmed.raza1054@gmail.com>
2023-09-15 02:16:58 +0500

    Calculator app button value
    Calculator app button value

--- JavaScript-Assignment/JS-Quiz/Calculator/app.js
+++ JavaScript-Assignment/JS-Quiz/Calculator/app.js
@@ -1,1 +1,52 @@
-// function numbers()
+var currentInput = "";
+var currentOperation = "";
+var previousInput = "";
+
+function numbers(value) {
+  currentInput += value;
+  document.getElementById("screen").value = currentInput;
+}
+
+function operationSign(value) {
+  currentInput += value;
+  document.getElementById("screen").value = currentInput;
+}
+
+// function calculate() {
+//   let result = 0;
+//   const num1 = parseFloat(previousInput);
+//   const num2 = parseFloat(currentInput);
+
+//   switch (currentOperation) {
+//     case "+":
+//       result = num1 + num2;
+//       break;
+//     case "-":
+//       result = num1 - num2;
+//       break;
+//     case "*":
+//       result = num1 * num2;
+//       break;
+//     case "/":
+//       if (num2 !== 0) {
+//         result = num1 / num2;
+//       } else {
+//         result = "Error: Division by zero";
+//       }
+//       break;
+//     default:
+//       result = "Error: Invalid operation";
+//   }
+
+//   document.getElementById("screen").value = result;
+//   currentInput = result.toString();
+//   previousInput = "";
+//   currentOperation = "";
+// }
+
+function clearScreen() {
+  currentInput = "";
+  currentOperation = "";
+  previousInput = "";
+  document.getElementById("screen").value = "";
+}
