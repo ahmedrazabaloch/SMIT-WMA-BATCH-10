fd48974058675448fa7a1c15582eae2107e71edd
Ahmed <ahmed.raza1054@gmail.com>
2023-09-17 23:18:43 +0500

    calculator updating
    calculator updating

--- JavaScript-Assignment/JS-Quiz/Calculator/app.js
+++ JavaScript-Assignment/JS-Quiz/Calculator/app.js
@@ -42,3 +42,19 @@ function clearScreen() {
   previousInput = "";
   document.getElementById("screen").value = "";
 }
+
+// var str = "5+5-";
+
+// var newInput = "10";
+
+// var last = str[str.length - 1];
+
+// var arr = ["+", "-", "*", "/"];
+
+// if (arr.indexOf(last) !== -1 && arr.indexOf(newInput) !== -1) {
+//   str = str.slice(0, -1) + newInput;
+// } else {
+//   str += newInput;
+// }
+
+// console.log(str);
