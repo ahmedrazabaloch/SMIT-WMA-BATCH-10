e786cb7bd7d18dffa020eb14c4551250a55316ee
Ahmed <ahmed.raza1054@gmail.com>
2023-09-18 13:01:37 -0500

    lamp on off task
    lamp on off task

--- JavaScript-Assignment/JS-Quiz/Tricky Question/app.js
+++ JavaScript-Assignment/JS-Quiz/Tricky Question/app.js
@@ -345,23 +345,6 @@
 // }
 // console.log(arr);
 
-//                                                     >>>>>>>>>>>>>>>>> Correct the code <<<<<<<<<<<<<<<<<<<<
-
-// var str = "5+5-";
-
-// var newInput = "10";
-
-// var last = str[str.length - 1];
-
-// var arr = ["+", "-", "*", "/"];
-
-// if (arr.indexOf(last) !== -1 && arr.indexOf(newInput) !== -1) {
-//   str = str.slice(0, -1) + newInput;
-// } else {
-//   str += newInput;
-// }
-
-// console.log(str);
 
 //                                        >>>>>>>>>>>>>>>>>>>>>>>>> Anagrams Words <<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
