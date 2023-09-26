7b00c35f664cd593247e9465ab62ae7b09b6f2e7
Ahmed <ahmed.raza1054@gmail.com>
2023-09-27 01:27:10 +0500

    both page are connected
    both page are connected

--- JavaScript-Assignment/JS-Quiz/chitChat/app.js
+++ JavaScript-Assignment/JS-Quiz/chitChat/app.js
@@ -1,17 +1,18 @@
 function formSubmission() {
-  var userName = document.getElementById("userName");
+  var userName = document.getElementById("userName").value;
   var signup = document.getElementById("signup-container");
   var container = document.getElementById("container");
   signup.style.display = "none";
   container.style.display = "block";
-  console.log(userName.value);
+  var displayName = document.getElementById("displayName");
+  displayName.innerHTML = userName;
 }
 
-function submit() {
-  var para = document.getElementById("para");
-  var posting = document.getElementById("posting");
-  para.innerHTML = posting.value;
-  posting.value = "";
-  console.log(posting);
-  console.log(para);
-}
+// function submit() {
+//   var para = document.getElementById("para");
+//   var posting = document.getElementById("posting");
+//   para.innerHTML = posting.value;
+//   posting.value = "";
+//   console.log(posting);
+//   console.log(para);
+// }
