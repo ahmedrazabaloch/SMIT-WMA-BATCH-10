ad5f16180233a7a54e3fb7e1d8a65a7a4cd5cbba
Ahmed <ahmed.raza1054@gmail.com>
2023-09-27 03:02:36 +0500

    functionality ready
    functionality ready just styling left

--- JavaScript-Assignment/JS-Quiz/chitChat/app.js
+++ JavaScript-Assignment/JS-Quiz/chitChat/app.js
@@ -1,18 +1,28 @@
-function formSubmission() {
-  var userName = document.getElementById("userName").value;
-  var signup = document.getElementById("signup-container");
-  var container = document.getElementById("container");
-  signup.style.display = "none";
-  container.style.display = "block";
-  var displayName = document.getElementById("displayName");
-  displayName.innerHTML = userName;
-}
+// function formSubmission() {
 
-// function submit() {
-//   var para = document.getElementById("para");
-//   var posting = document.getElementById("posting");
-//   para.innerHTML = posting.value;
-//   posting.value = "";
-//   console.log(posting);
-//   console.log(para);
+//   var userName = document.getElementById("userName").value;
+//   var signup = document.getElementById("signup-container");
+//   var container = document.getElementById("container");
+
+//   signup.style.display = "none";
+
+//   container.style.display = "block";
+
+//   var displayName = document.getElementById("displayName");
+
+//   displayName.innerHTML = "@" + userName;
 // }
+
+function submit() {
+  var posting = document.getElementById("posting");
+  var output = document.getElementById("output");
+
+  var newPara = document.createElement("p");
+  newPara.textContent = posting.value;
+
+  output.appendChild(newPara);
+
+  posting.value = "";
+
+  console.log(posting);
+}
